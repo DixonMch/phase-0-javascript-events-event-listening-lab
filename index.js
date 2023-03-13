e291c5b756969d0502782eacc7eb169b717a6e2c
DixonMch <dixonmogaka@gmail.com>
2023-03-13 04:07:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
 }
+function addingEventListener() {
+    const input = document.getElementById("button");
+  
+    function clickAlert() {
+      alert("Clicked!");
+    }
+  
+    input.addEventListener("click", clickAlert);
+  }
+  
+  addingEventListener();
